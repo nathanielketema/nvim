30727c886df7dec535d54b5b541fb54928a05188
Nathaniel Ketema <nathanielketema@gmail.com>
2024-07-06 17:56:55 -0400

    go to reference

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -60,7 +60,7 @@ return {
 			vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
