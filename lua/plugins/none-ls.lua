ba0a168bbf0d6344138095796436757ed67dc037
Nathaniel Ketema <nathanielketema@gmail.com>
2024-07-07 14:30:10 -0400

    added mypy and ruff

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,6 +10,8 @@ return {
                 null_ls.builtins.completion.spell,
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.black,
+                null_ls.builtins.diagnostics.mypy,
+                null_ls.builtins.diagnostics.ruff,
                 null_ls.builtins.formatting.isort,
             },
         })
