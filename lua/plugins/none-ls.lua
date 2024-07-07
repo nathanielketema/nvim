9aceeef01e5656d42ee1d7b575ca6eedbd4a9ec1
Nathaniel Ketema <nathanielketema@gmail.com>
2024-07-07 13:38:43 -0400

    removed erb_lint

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,6 @@ return {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.completion.spell,
                 null_ls.builtins.formatting.prettier,
-                null_ls.builtins.diagnostics.erb_lint,
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
             },
