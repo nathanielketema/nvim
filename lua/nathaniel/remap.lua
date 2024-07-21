6caa53ac3e23ce064397f646112dc5bee577a9e9
Nathaniel Ketema <nathanielketema@gmail.com>
2024-07-21 15:17:55 -0400

    vim-floatterm

--- lua/nathaniel/remap.lua
+++ lua/nathaniel/remap.lua
@@ -52,10 +52,6 @@ vim.keymap.set("n", "<C-j>", ":cnext<CR>")
 vim.keymap.set("n", "<C-k>", ":cprevious<CR>")
 vim.keymap.set("n", "K", ":vim /<C-R><C-W>/ **<CR>:copen<CR>")
 
--- FloatTerm
-vim.keymap.set("n", "<leader>tt", ":FloatermNew --width=0.7 --height=0.7<CR>")
-vim.keymap.set("n", "<leader>gg", ":FloatermNew --width=0.9 --height=0.9 lazygit<CR>")
-vim.keymap.set("n", "<leader>lf", ":FloatermNew --width=0.8 --height=0.8 lf<CR>")
 
 
 -- LaTeX
--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,3 +1,9 @@
 return {
     "voldikss/vim-floaterm",
+
+    config = function()
+        vim.keymap.set("n", "<leader>ft", ":FloatermNew --width=0.7 --height=0.7<CR>")
+        vim.keymap.set("n", "<leader>lg", ":FloatermNew --width=0.9 --height=0.9 lazygit<CR>")
+        vim.keymap.set("n", "<leader>lf", ":FloatermNew --width=0.8 --height=0.8 lf<CR>")
+    end,
 }
