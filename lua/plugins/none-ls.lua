aaad9fb78263d19f07c76f56ab3d7d92159c2b38
Nathaniel Ketema <nathanielketema@gmail.com>
2024-08-01 11:36:18 -0400

    removed latexindent formatter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -13,7 +13,6 @@ return {
                 null_ls.builtins.diagnostics.mypy,
                 null_ls.builtins.diagnostics.ruff,
                 null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.latexindent,
             },
         })
 
