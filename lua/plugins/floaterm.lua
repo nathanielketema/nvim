c73defede8fc15491ee9d559893379774cc585d2
Nathaniel Ketema <nathanielketema@gmail.com>
2025-01-02 18:08:25 -0600

    added yazi to floaterm

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -4,6 +4,6 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>ft", ":FloatermNew --width=0.7 --height=0.7<CR>")
         vim.keymap.set("n", "<leader>lg", ":FloatermNew --width=0.9 --height=0.9 lazygit<CR>")
-        vim.keymap.set("n", "<leader>lf", ":FloatermNew --width=0.8 --height=0.8 lf<CR>")
+        vim.keymap.set("n", "<leader>y", ":FloatermNew --width=0.7 --height=0.8 yazi<CR>")
     end,
 }
